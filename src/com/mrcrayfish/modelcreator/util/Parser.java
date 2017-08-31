263695fb250dd4884f0be7c32fbdbda5b6458612
mennovf <mennovanfrachem@hotmail.com>
2017-08-31 18:11:24 +0200

    Use the same localization for Double inputs as is used in formatting output. This caused a weird ux experience where changing a number after the decimal seperator wasn't accepted as a valid change.

--- src/com/mrcrayfish/modelcreator/util/Parser.java
+++ src/com/mrcrayfish/modelcreator/util/Parser.java
@@ -1,15 +1,19 @@
 package com.mrcrayfish.modelcreator.util;
 
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
 public class Parser
 {
+	private static DecimalFormat df = new DecimalFormat("#.#");
 	public static double parseDouble(String text, double def)
 	{
 		double value;
 		try
 		{
-			value = Double.parseDouble(text);
+			value = df.parse(text).doubleValue();
 		}
-		catch (NumberFormatException e)
+		catch (ParseException e)
 		{
 			value = def;
 		}

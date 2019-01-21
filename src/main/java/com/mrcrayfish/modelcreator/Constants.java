21eecb15d8fe56a397f487e2bce896597718f687
MrCrayfish <mrcrayfish@hotmail.com>
2019-01-21 14:52:53 +1030

    Updated version

--- src/main/java/com/mrcrayfish/modelcreator/Constants.java
+++ src/main/java/com/mrcrayfish/modelcreator/Constants.java
@@ -3,7 +3,7 @@ package com.mrcrayfish.modelcreator;
 public class Constants
 {
     public static final String NAME = "MrCrayfish's Model Creator";
-    public static final String VERSION = "0.5.0";
+    public static final String VERSION = "0.6.0";
 
     public static final String URL_DONATE = "https://www.patreon.com/mrcrayfish?ty=h";
     public static final String URL_TWITTER = "https://www.twitter.com/MrCraayfish";
